6ce0d772eed8a7f5f6aed3323b2f9a1855ac1a0a
Manos Liakos <=>
2020-09-24 00:34:03 +0300

    Cleanup

--- src/components/Autosuggest/InfoArea/InfoArea.js
+++ src/components/Autosuggest/InfoArea/InfoArea.js
@@ -5,74 +5,63 @@ import classes from "./InfoArea.module.css";
 
 const InfoArea = props => {
 	const heroObject = props.heroObject;
-	let container;
 
-	if (true) {
-		container = (
-			<table className={classes.infoArea}>
-				<tbody>
-					<tr>
-						<td className={classes.nameRow} colSpan={"2"}>
-							<strong>
-								{heroObject.name ||
-									"Find & select a hero to display its properties!"}
-							</strong>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<strong>Gender</strong>
-						</td>
-						<td>{heroObject.gender || "-"}</td>
-					</tr>
-					<tr>
-						<td>
-							<strong>Birth Year</strong>
-						</td>
-						<td>{heroObject.birth_year || "-"}</td>
-					</tr>
-					<tr>
-						<td>
-							<strong>Height</strong>
-						</td>
-						<td>{heroObject.height || "-"}</td>
-					</tr>
-					<tr>
-						<td>
-							<strong>Mass</strong>
-						</td>
-						<td>{heroObject.mass || "-"}</td>
-					</tr>
-					<tr>
-						<td>
-							<strong>Hair color</strong>
-						</td>
-						<td>{heroObject.hair_color || "-"}</td>
-					</tr>
-					<tr>
-						<td>
-							<strong>Skin color</strong>
-						</td>
-						<td>{heroObject.skin_color || "-"}</td>
-					</tr>
-					<tr>
-						<td>
-							<strong>Eye color</strong>
-						</td>
-						<td>{heroObject.eye_color || "-"}</td>
-					</tr>
-				</tbody>
-			</table>
-		);
-	} else {
-		container = (
-			<table>
-				<row></row>
-			</table>
-		);
-	}
-
-	return container;
+	return (
+		<table className={classes.infoArea}>
+			<tbody>
+				<tr>
+					<td className={classes.nameRow} colSpan={"2"}>
+						<strong>
+							{heroObject.name ||
+								"Find & select a hero to display its properties!"}
+						</strong>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<strong>Gender</strong>
+					</td>
+					<td>{heroObject.gender || "-"}</td>
+				</tr>
+				<tr>
+					<td>
+						<strong>Birth Year</strong>
+					</td>
+					<td>{heroObject.birth_year || "-"}</td>
+				</tr>
+				<tr>
+					<td>
+						<strong>Height</strong>
+					</td>
+					<td>{heroObject.height || "-"}</td>
+				</tr>
+				<tr>
+					<td>
+						<strong>Mass</strong>
+					</td>
+					<td>{heroObject.mass || "-"}</td>
+				</tr>
+				<tr>
+					<td>
+						<strong>Hair color</strong>
+					</td>
+					<td>{heroObject.hair_color || "-"}</td>
+				</tr>
+				<tr>
+					<td>
+						<strong>Skin color</strong>
+					</td>
+					<td>{heroObject.skin_color || "-"}</td>
+				</tr>
+				<tr>
+					<td>
+						<strong>Eye color</strong>
+					</td>
+					<td>{heroObject.eye_color || "-"}</td>
+				</tr>
+			</tbody>
+		</table>
+	);
 };
 
 InfoArea.propTypes = {
